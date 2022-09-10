b6cfe32d59ffc18aea258684636f79691b44d56d
alexander <deiner.alexander@yandex.ru>
2022-09-10 12:32:40 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from application.people import get_employees
+from application.salary import calculate_salary
+
+
+def main():
+    get_employees(),
+    calculate_salary()
+
+
+if __name__ == '__main__':
+    main()
