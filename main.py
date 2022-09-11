559ad9ef5fadbd43c2017cb290e46f72130d8b2f
alexander <deiner.alexander@yandex.ru>
2022-09-11 11:28:19 +0300

    final

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from application.people import get_employees
 from application.salary import calculate_salary
+from datetime import date
 
 
 def main():
-    get_employees(),
+    print(date.today())
+    get_employees()
     calculate_salary()
 
 
